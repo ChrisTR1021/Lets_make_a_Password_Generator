80e2fef6377f526b55ee8ed7de6c6d6053a18e88
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-23 00:16:34 -0400

    attempted functions being written

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,23 @@
 // Assignment Code
 const characterlimit = document.getElementById ('characterlimit')
+const adduppercaseElement = document.getElementById ('adduppercase')
 const generatordetails = document.getElementById
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function synccharacterlimit(e) {
+  const value = e.target.value
+  characterlimit.value = value
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 characterlimit.addEventListener("input", synccharacterlimit);
 generatordetails.addEventListener('submit', e => {
   e.preventDefault()
+  const characterlimit = characterlimit.value
+  const adduppercase = adduppercaseElement
+  const password = passwordbeinggenerated(characterlimit, uppercaseincluded,
+    symbolsincluded, numbersincluded)
 })
