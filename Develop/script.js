8c59c2f4e87eca09e4ff9661e34cfe9d1ada93b0
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-23 03:00:45 -0400

    charcodes added but they don't work

--- Develop/script.js
+++ Develop/script.js
@@ -6,12 +6,28 @@ const addnumbers = document.getElementById('addnumbers');
 const addsymbols = document.getElementById('addsymbols')
 
 
-// Write password to the #password input
+const uppercase_char_codes = arraylowerandhigher(65, 90)
+const lowercase_char_codes = arraylowerandhigher(97, 122)
+const symbol_char_codes = arraylowerandhigher(33, 47).concat(
+  arraylowerandhigher(58, 64)
+)
+const number_char_codes = arraylowerandhigher(48, 57)
 
 function synccharacterlimit(e) {
   const value = e.target.value
   characterlimit.value = value
 }
+function passwordgenerated(characterlimit, addUppercase, addnumbers, 
+  addsymbols) {
+   String.fromCharCode(65)
+  }
+
+  function arraylowerandhigher(low, high) {
+    for (let i = low; i <= high; i++) {
+
+    }
+    return array
+  }
 
 
 // Add event listener to generate button
