d29453db14f4f25ab57d30ea1f34219d0561a7df
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-22 20:35:10 -0400

    added box styling and began javascript functions and event listeners

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 // Assignment Code
+const characterlimit = document.getElementById ('characterlimit')
+const generatordetails = document.getElementById
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,8 +9,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+characterlimit.addEventListener("input", synccharacterlimit);
+generatordetails.addEventListener('submit', e => {
+  e.preventDefault()
+})
