cea641d0f43d95cf5846b8fb2e65fadcc0543ea6
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-23 19:00:40 -0400

    redid arrays removed functions as they weren't operable

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,48 @@
 // Assignment Code
-const characterlimit = document.getElementById('characterlimit');
-const adduppercaseElement = document.getElementById('adduppercase');
-const generatordetails = document.getElementById('generatordetails');
-const addnumbers = document.getElementById('addnumbers');
-const addsymbols = document.getElementById('addsymbols')
+const characterLimitElement = document.getElementById("characterLimit");
+const addUppercaseElement = document.getElementById("addUppercase");
+const generatordetails = document.getElementById("generatordetails");
+const generateBtn = document.getElementById("generate");
+const addNumbersElement = document.getElementById("addNumbers");
+const addSymbolsElement = document.getElementById("addSymbols");
 
+const uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const symbolChars = ["!", "@", "#", "$", "%", "^", "&", "*",];
+const numberChar = ["0","1","2","3","4","5","6","7","8","9"];
 
-const uppercase_CHAR_CODES = arraylowerandhigher(65, 90)
-const lowercase_CHAR_CODES = arraylowerandhigher(97, 122)
-const symbol_CHAR_CODES = arraylowerandhigher(33, 47).concat(
-  arraylowerandhigher(58, 64)
-)
-const number_char_codes = arraylowerandhigher(48, 57)
+// function passwordBeingGenerated(
+//   characterlimit,
+//   addUppercase,
+//   addnumbers,
+//   addsymbols
+// ) {
+//   let charcodes = lowercase_CHAR_CODES;
+//   if (addUppercase) charcodes = chardcodes.concat(uppercase_CHAR_CODES);
+//   if (addnumbers) charcodes = charcodes(number_char_codes);
+//   if (addsymbols) charcodes = chardcodes.concat(symbol_CHAR_CODES);
+//   return charcodes 
+// }
 
-function synccharacterlimit(e) {
-  const value = e.target.value
-  characterlimit.value = value
-}
-function passwordgenerated(characterlimit, addUppercase, addnumbers, 
-  addsymbols) {
-   let charcodes = lowercase_CHAR_CODES
-   if (addUppercase) charcodes = chardcodes.concat
-   (uppercase_CHAR_CODES)
-   if (addnumbers) charcodes = charcodes(number_char_codes)
-   if (addsymbols) charcodes = chardcodes.concat(symbol_CHAR_CODES)
-  }
-
-  function arraylowerandhigher(low, high) {
-    const array = [];
-    for (let i = low; i <= high; i++) {
-      array.push(i);
-    }
-    return array;
-  }
-
+// function arraylowerandhigher(low, high) {
+//   const array = [];
+//   for (let i = low; i <= high; i++) {
+//     array.push(i);
+//   }
+//   return array;
+// }
 
 // Add event listener to generate button
-characterlimit.addEventListener("input", synccharacterlimit);
-generatordetails.addEventListener('submit', e => {
+generateBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const limit = characterlimit.value; // Use a different name for the local variable
-  const addUppercase = adduppercaseElement.checked
-  const numbersincluded = addnumbersElement.checked
-  const symbolsincluded = addsymbolsElement.checked 
-  const password = passwordbeinggenerated(limit, addUppercase, symbolsincluded, 
-    numbersincluded);
+  const limit = characterLimitElement.value;
+  const symbolsincluded = addSymbolsElement.checked;
+  const addUppercase = addUppercaseElement.checked;
+  const numbersincluded = addNumbersElement.checked;
+  const password = passwordBeingGenerated(
+    limit,
+    addUppercase,
+    symbolsincluded,
+    numbersincluded
+  );
 });
