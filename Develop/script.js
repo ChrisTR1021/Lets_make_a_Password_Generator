2975e4dd43aca34dba4dd763ecbbc0f2d0ac8844
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-23 01:43:54 -0400

    event listener, const reworked

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Assignment Code
-const characterlimit = document.getElementById ('characterlimit')
-const adduppercaseElement = document.getElementById ('adduppercase')
-const generatordetails = document.getElementById
+const characterlimit = document.getElementById('characterlimit');
+const adduppercaseElement = document.getElementById('adduppercase');
+const generatordetails = document.getElementById('generatordetails');
+const addnumbers = document.getElementById('addnumbers');
+const addsymbols = document.getElementById('addsymbols')
 
 
 // Write password to the #password input
@@ -15,9 +17,10 @@ function synccharacterlimit(e) {
 // Add event listener to generate button
 characterlimit.addEventListener("input", synccharacterlimit);
 generatordetails.addEventListener('submit', e => {
-  e.preventDefault()
-  const characterlimit = characterlimit.value
-  const adduppercase = adduppercaseElement
-  const password = passwordbeinggenerated(characterlimit, uppercaseincluded,
-    symbolsincluded, numbersincluded)
-})
+  e.preventDefault();
+  const limit = characterlimit.value; // Use a different name for the local variable
+  const addUppercase = adduppercaseElement.checked
+  const numbersincluded = addnumbersElement.checked
+  const symbolsincluded = addsymbolsElement.checked 
+  const password = passwordbeinggenerated(limit, addUppercase, symbolsincluded, numbersincluded);
+});
