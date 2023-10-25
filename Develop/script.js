421c13f91f257683726f31b2ff41559dd984423c
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-25 14:38:04 -0400

    added console log and fixed functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,29 @@
 // Assignment Code
-const characterLimitElement = document.getElementById("characterLimit");
-const addUppercaseElement = document.getElementById("addUppercase");
-const addLowercaseElement = document.getElementById("addLowercase");
-const generatordetails = document.getElementById("generatordetails");
-const generateBtn = document.getElementById("generate");
-const addNumbersElement = document.getElementById("addNumbers");
-const addSymbolsElement = document.getElementById("addSymbols");
+var characterLimitElement = document.getElementById("characterLimit");
+var addUppercaseElement = document.getElementById("addUppercase");
+var addLowercaseElement = document.getElementById("addLowercase");
+var generatordetails = document.getElementById("generatordetails");
+var generateBtn = document.getElementById("generate");
+var addNumbersElement = document.getElementById("addNumbers");
+var addSymbolsElement = document.getElementById("addSymbols");
 
-const uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
-const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const symbolChars = ["!", "@", "#", "$", "%", "^", "&", "*",];
-const numberChar = ["0","1","2","3","4","5","6","7","8","9"];
-
+var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var symbolChars = ["!", "@", "#", "$", "%", "^", "&", "*",];
+var numberChar = ["0","1","2","3","4","5","6","7","8","9"];
+var possible_password = []
 function generatePassword() {
   console.log(addSymbolsElement.checked);
-  const limit = Number(characterLimitElement.value);
-  const symbolsincluded = addSymbolsElement.checked;
-  const addUppercase = addUppercaseElement.checked;
-  const addLowercase = addLowercaseElement.checked;
-  const numbersincluded = addNumbersElement.checked;
+  var limit = Number(characterLimitElement.value);
+  var symbolsincluded = addSymbolsElement.checked;
+  var addUppercase = addUppercaseElement.checked;
+  var addLowercase = addLowercaseElement.checked;
+  var numbersincluded = addNumbersElement.checked;
   console.log(symbolsincluded);
   if (symbolsincluded === true) {
+    console.log("before", possible_password)
     possible_password = possible_password.concat(symbolChars);
+    console.log("after", possible_password)
   }
   if (addUppercase === true) {
     possible_password = possible_password.concat(uppercaseChars);
@@ -43,7 +45,7 @@ function generatePassword() {
   // Add event listener to generate button
 generateBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const password = generatePassword()
+  var password = generatePassword()
   passwordElement = document.querySelector("#password")
   passwordElement.value = password;
 });
