edba48c77b16e20c59becf00c760a52181bd09e6
ChrisTR1021 <christianridgell1021@gmail.com>
2023-10-25 03:37:15 -0400

    re added characters for function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 const characterLimitElement = document.getElementById("characterLimit");
 const addUppercaseElement = document.getElementById("addUppercase");
+const addLowercaseElement = document.getElementById("addLowercase");
 const generatordetails = document.getElementById("generatordetails");
 const generateBtn = document.getElementById("generate");
 const addNumbersElement = document.getElementById("addNumbers");
@@ -11,38 +12,21 @@ const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 const symbolChars = ["!", "@", "#", "$", "%", "^", "&", "*",];
 const numberChar = ["0","1","2","3","4","5","6","7","8","9"];
 
-// function passwordBeingGenerated(
-//   characterlimit,
-//   addUppercase,
-//   addnumbers,
-//   addsymbols
-// ) {
-//   let charcodes = lowercase_CHAR_CODES;
-//   if (addUppercase) charcodes = chardcodes.concat(uppercase_CHAR_CODES);
-//   if (addnumbers) charcodes = charcodes(number_char_codes);
-//   if (addsymbols) charcodes = chardcodes.concat(symbol_CHAR_CODES);
-//   return charcodes 
-// }
 
-// function arraylowerandhigher(low, high) {
-//   const array = [];
-//   for (let i = low; i <= high; i++) {
-//     array.push(i);
-//   }
-//   return array;
-// }
+function generatePassword() {
+  const limit = characterLimitElement.value;
+  const symbolsincluded = addSymbolsElement.checked;
+  const addUppercase = addUppercaseElement.checked;
+  const addLowercase = addLowercaseElement.checked;
+  const numbersincluded = addNumbersElement.checked;
+  let newPassword = "password"
+  return newPassword;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const limit = characterLimitElement.value;
-  const symbolsincluded = addSymbolsElement.checked;
-  const addUppercase = addUppercaseElement.checked;
-  const numbersincluded = addNumbersElement.checked;
-  const password = passwordBeingGenerated(
-    limit,
-    addUppercase,
-    symbolsincluded,
-    numbersincluded
-  );
+  const password = generatePassword()
+  passwordElement = document.querySelector("#password")
+  passwordElement.value = password;
 });
